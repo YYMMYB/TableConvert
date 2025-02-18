bd65e17bd0fadc7aca5acd589b61392957b87c08
yym <1329112803@qq.com>
2025-02-18 19:10:59 +0800

    _

--- TableConvertor/Format.cs
+++ TableConvertor/Format.cs
@@ -24,9 +24,10 @@ public class Line {
     }
 }
 
-public class Formate {
+public class Format {
     public Line line;
     public Value? value;
+    public int size;
 
     // 跨行状态
     public bool finished;
@@ -35,22 +36,42 @@ public class Formate {
     public bool valid;
 
     public void ReadLine(Line line) {
-        Reset(line);
+        OnNewLine(line);
         var column = 0;
         ParseValue(ref column);
     }
 
-    public virtual void Reset(Line line) {
+    public virtual void OnNewLine(Line line) {
+        this.line = line;
         valid = true;
     }
 
     public virtual void ParseValue(ref int column) { }
+
+    public virtual void AlignColumn(int start, ref int column) {
+        if (column < start) {
+            throw new Exception();
+        } else if (column < start + size) {
+            for (var c = column; c < start + size; c++) {
+                if (line.cells[c].k == CellData.Kind.Value) {
+                    valid = false;
+                    break;
+                }
+            }
+            column = start + size;
+        } else if (column == start + size) {
+
+        } else if (column > start + size) {
+            throw new Exception();
+        }
+
+    }
+
 }
 
-public class OneCell : Formate {
-    public override void Reset(Line line) {
-        base.Reset(line);
-        this.line = line;
+public class OneCell : Format {
+    public override void OnNewLine(Line line) {
+        base.OnNewLine(line);
     }
     public override void ParseValue(ref int column) {
         base.ParseValue(ref column);
@@ -69,46 +90,52 @@ public class OneCell : Formate {
     }
 }
 
-public class VTemplate : Formate {
+public class VTemplate : Format {
 
     public List<HTemplate> templates = new();
     public int cur;
-    public override void Reset(Line line) {
-        base.Reset(line);
-        this.line = line;
-        templates[cur].Reset(line);
+    public override void OnNewLine(Line line) {
+        base.OnNewLine(line);
+        templates[cur].OnNewLine(line);
     }
 
     public override void ParseValue(ref int column) {
         base.ParseValue(ref column);
+        var start = column;
         if (!finished) {
             templates[cur].ParseValue(ref column);
             if (templates[cur].finished) {
                 cur += 1;
             }
         }
+
+        AlignColumn(start, ref column);
+
         if (cur >= templates.Count) {
+            // 收集
+            var q = from item in templates
+                    select item.value;
+            value = new ListValue(q.ToList());
             finished = true;
         }
     }
 }
 
-public class HTemplate : Formate {
-    public Line line;
-    public Value value;
+public class HTemplate : Format {
 
+    public List<Format> items = new();
 
-    public List<Formate> items = new();
-
-    public override void Reset(Line line) {
-        this.line = line;
+    public override void OnNewLine(Line line) {
+        base.OnNewLine(line);
         foreach (var item in items) {
-            item.Reset(line);
+            item.OnNewLine(line);
         }
     }
 
     public override void ParseValue(ref int column) {
         base.ParseValue(ref column);
+        var start = column;
+
         finished = true;
         valid = true;
         foreach (var item in items) {
@@ -116,5 +143,53 @@ public class HTemplate : Formate {
             finished = finished && item.finished;
             valid = valid && item.valid;
         }
+
+        AlignColumn(start, ref column);
+
+        if (finished) {
+            // 收集
+            var q = from item in items
+                    select item.value;
+            value = new ListValue(q.ToList());
+        }
+
+    }
+}
+
+public class Switch : Format {
+    public bool known;
+    // 挡位. 表示当前是走的哪个分支.
+    public Value? gear;
+    public Format gearFormat;
+    public Dictionary<Value, Format> branches = new();
+
+    public override void OnNewLine(Line line) {
+        base.OnNewLine(line);
+
+        gearFormat.OnNewLine(line);
+        // 不知道用谁, 就都设置
+        foreach (var (g, b) in branches) {
+            b.OnNewLine(line);
+        }
+    }
+
+    public override void ParseValue(ref int column) {
+        base.ParseValue(ref column);
+
+        var start = column;
+        if (!gearFormat.finished) {
+            gearFormat.ParseValue(ref column);
+            if (!gearFormat.valid) { valid = false; } else {
+                if (gearFormat.finished) {
+                    gear = gearFormat.value;
+                }
+            }
+        }
+
+        if (gearFormat.finished) {
+            branches[gear].ParseValue(ref column);
+        }
+
+        AlignColumn(start, ref column);
     }
 }
