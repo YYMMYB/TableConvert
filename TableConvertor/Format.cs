ce5e08621c7dfa89a939dbe6900a9535c72c8c37
yym <1329112803@qq.com>
2025-02-22 19:58:36 +0800

    这个应该是没问题的, 重写, 砍掉默认值功能, 并改用一个状态

--- TableConvertor/Format.cs
+++ TableConvertor/Format.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -27,29 +28,144 @@ public class Line {
 
 public class Format {
     public int size;
+    // HVList 为 true
+    // HVTuple 为 false
+    public bool ResetChildren { get; set; }
 
     public Line line;
+    public Value? value;
 
-    // 和 line 一样, 只是个引用, 函数的通用参数.
-    // state真正的存储位置, 会根据实际情况有所不同.
-    // 可能随时变化. 因为 Format 可以重用.
-    // 比如 HFormat 读取列表的时候, 每个新项都会重新设置正确的 state
-    public required State state;
-
-
-    public void ReadLine(Line line) {
-        OnNewLine(line);
-        var column = 0;
-        ParseValue(ref column);
+    public Format? keyTemplate;
+    public Format? template;
+    public List<Format>? children;
+    public int curChildIndex;
+    public Format CurChild {
+        get {
+            if (children == null)
+                throw new Exception();
+            return children[curChildIndex];
+        }
+        set {
+            if (children == null)
+                throw new Exception();
+            children[curChildIndex] = value;
+        }
     }
 
-    public virtual void OnNewLine(Line line) {
-        this.line = line;
+    public bool firstParse;
+    public bool valid;
+    public bool finished;
+    public bool breakable;
+    public bool isEmpty;
+    public bool allValue;
+
+    public bool meetEnd;
+    public bool meetDefault;
+
+    public bool defaultMode;
+    public Value? defaultValue;
+
+    public Format? history;
+
+    // 一般模式: 深拷贝 value 和 children, 重置 history, savedxxx
+    // 历史记录: 保留 value 和 children 的引用, 保留 history, 设置 savedxxx 属性
+    // 目前来说 历史记录 模式没有用到. 也没改bug, 肯定是错的(已知的,至少没处理 curChildIndex). 
+    public virtual Format Clone(bool useForHistory = false) {
+        var res = new Format();
+        res.ClonedBy(this, useForHistory);
+        return res;
+    }
+
+    public virtual void ClonedBy(Format origin, bool useForHistory = false) {
+        size = origin.size;
+        ResetChildren = origin.ResetChildren;
+
+        line = origin.line;
+        if (useForHistory) {
+            value = origin.value;
+        } else {
+            value = origin.value?.Clone();
+        }
+
+        keyTemplate = origin.keyTemplate?.Clone();
+        template = origin.template?.Clone();
+        if (origin.children != null) {
+            var ls = new List<Format>();
+            if (useForHistory) {
+                children = origin.children;
+            } else {
+                foreach (var ch in origin.children) {
+                    ls.Add(ch.Clone());
+                }
+                children = ls;
+                curChildIndex = origin.curChildIndex;
+            }
+        }
+
+        firstParse = origin.firstParse;
+        valid = origin.valid;
+        finished = origin.finished;
+        breakable = origin.breakable;
+        isEmpty = origin.isEmpty;
+        allValue = origin.allValue;
+
+        meetEnd = origin.meetEnd;
+        meetDefault = origin.meetDefault;
+
+        defaultMode = origin.defaultMode;
+        defaultValue = origin.defaultValue;
+
+        if (useForHistory) {
+            history = origin.history;
+        } else {
+            history = origin.history;
+        }
+    }
+
+    // 不会保留历史记录, 完全的新项.
+    public virtual void Reset() {
+        // 保持不变的
+        //size = size;
+        //resetChildren = resetChildren;
+
+        //line = line;
+        value = null;
+
+        //template = template;
+        if (children != null) {
+            if (ResetChildren) {
+                children = new List<Format>();
+            }
+            curChildIndex = 0;
+        } else {
+            curChildIndex = -1;
+        }
+
+        firstParse = true;
+        valid = true;
+        finished = false;
+        breakable = false;
+        isEmpty = false;
+        allValue = false;
+
+        meetEnd = false;
+        meetDefault = false;
+
+        defaultMode = false;
+        defaultValue = null;
+
+        history = null;
+
+        keyTemplate?.Reset();
+        template?.Reset();
+        foreach (var ch in children) {
+            ch.Reset();
+        }
     }
 
     public virtual void ParseValue(ref int column) {
-        if (!state.valid) {
-            throw new Exception();
+        if (finished) {
+            ParseNonValue(ref column);
         }
     }
 
@@ -59,7 +175,7 @@ public class Format {
         } else if (column < start + size) {
             for (var c = column; c < start + size; c++) {
                 if (line.cells[c].k == CellData.Kind.Value) {
-                    state.valid = false;
+                    valid = false;
                     break;
                 }
             }
@@ -70,208 +186,310 @@ public class Format {
             throw new Exception();
         }
     }
+
+    public virtual void ForceFinish() {
+        if (breakable) {
+            finished = true;
+            if (children != null) {
+                foreach (var ch in children) {
+                    if (!ch.finished) {
+                        ch.ForceFinish();
+                    }
+                    if (!ch.valid) {
+                        valid = false;
+                        return;
+                    }
+                }
+            }
+        } else {
+            valid = false;
+            return;
+        }
+    }
+
     public virtual void Collect() { }
 
-    public virtual void ClearState() { }
+    // 目前是子节点和值全都克隆, 可能(肯定)有优化办法, 共用值的List
+    public void Save() {
+        var h = Clone();
+    }
+    public void Restore() {
+        if (history != null) {
+            ClonedBy(history);
+        }
+    }
+    public void DropHistroy() {
+        history = null;
+    }
 
+    public void ReadLine(Line line) {
+        OnNewLine(line);
+        var column = 0;
+        ParseValue(ref column);
+    }
+
+    public void ParseNonValue(ref int column) {
+        for (; column < column + size; column++) {
+            switch (line.cells[column].k) {
+                case CellData.Kind.Value:
+                    valid = false;
+                    break;
+                case CellData.Kind.End:
+                    meetEnd = true;
+                    break;
+                case CellData.Kind.Default:
+                    valid = false;
+                    break;
+                case CellData.Kind.Placeholder:
+                    valid = false;
+                    break;
+                case CellData.Kind.Ignore:
+                    break;
+            }
+        }
+        valid = valid && !(meetEnd && meetDefault);
+    }
+
+    public virtual void OnNewLine(Line line) {
+        this.line = line;
+        template?.OnNewLine(line);
+        if (children != null) {
+            foreach (var child in children) {
+                child?.OnNewLine(line);
+            }
+        }
+    }
+
+    public void UpdateValueNullability() {
+        if (!defaultMode) {
+            if (firstParse) {
+                var allE = true;
+                var allV = true;
+                foreach (var ch in children) {
+                    allE = allE && ch.isEmpty;
+                    allV = allV && !ch.isEmpty;
+                }
+
+                if (!allE && !allV) {
+                    valid = false;
+                    return;
+                }
+
+                isEmpty = allE;
+                //allValue = allV;
+            } else {
+                foreach (var ch in children) {
+                    if (isEmpty != ch.isEmpty) {
+                        valid = false;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
 
 public class OneCell : Format {
-    public override void OnNewLine(Line line) {
-        base.OnNewLine(line);
+    public override Format Clone(bool useForHistory = false) {
+        var res = new OneCell();
+        res.ClonedBy(this, useForHistory);
+        return res;
     }
+
     public override void ParseValue(ref int column) {
         base.ParseValue(ref column);
-        if (state.finished) {
-            if (this.line.cells[column].k == CellData.Kind.Value) {
-                state.valid = false;
+        if (!finished) {
+            var cell = line.cells[column];
+            switch (cell.k) {
+                case CellData.Kind.Value:
+                    if (defaultMode) {
+                        value = cell.val;
+                    } else {
+                        value = cell.val;
+                        isEmpty = false;
+                    }
+                    break;
+                case CellData.Kind.Default:
+                    if (defaultMode) {
+                        valid = false;
+                    } else {
+                        meetDefault = true;
+                    }
+                    break;
+                case CellData.Kind.End:
+                    meetEnd = true; break;
+                case CellData.Kind.Ignore:
+                case CellData.Kind.Placeholder:
+                    if (defaultMode) {
+                        value = null;
+                    } else {
+                        value ??= cell.val;
+                        isEmpty = value == null;
+                    }
+                    break;
+            }
+            finished = true;
+            breakable = true;
+            column += 1;
+            ParseNonValue(ref column);
+        }
+        //if (!valid) { return; }
+    }
+}
+
+public class HList : Format {
+
+    //public List<Format> defaultChildren = new();
+
+    public override Format Clone(bool useForHistory = false) {
+        var res = new HList();
+        res.ClonedBy(this, useForHistory);
+        return res;
+    }
+
+    //public override void ClonedBy(Format origin, bool useForHistory = false) {
+    //    base.ClonedBy(origin, useForHistory);
+    //    var ls = new List<Format>();
+    //    foreach (var child in defaultChildren) {
+    //        ls.Add(child.Clone());
+    //    }
+    //    defaultChildren = ls;
+    //}
+
+    //public override void Reset() {
+    //    base.Reset();
+    //    defaultChildren = new();
+    //}
+
+    public override void ParseValue(ref int column) {
+        base.ParseValue(ref column);
+        if (!finished) {
+            if (template == null || children == null) {
+                throw new Exception();
+            }
+            curChildIndex = 0;
+            while (column + template.size <= size) {
+                if (CurChild == null) {
+                    var ch = template.Clone();
+                    if (curChildIndex == children.Count) {
+                        children.Add(ch);
+                    } else {
+                        CurChild = ch;
+                    }
+                }
+                CurChild.ParseValue(ref column);
+                if (!CurChild.valid) {
+                    valid = false;
+                    if (!valid) { return; }
+                    break;
+                }
+                curChildIndex++;
+            }
+
+
+            finished = true;
+            foreach (var ch in children) {
+                if (!ch.finished) {
+                    finished = false;
+                    break;
+                }
+            }
+            breakable = true;
+            foreach (var ch in children) {
+                if (!ch.breakable) {
+                    breakable = false;
+                    break;
+                }
+            }
+            meetEnd = false;
+            foreach (var ch in children) {
+                if (ch.meetEnd) {
+                    meetEnd = true;
+                    break;
+                }
+            }
+            meetDefault = false;
+            foreach (var ch in children) {
+                if (ch.meetDefault) {
+                    meetDefault = true;
+                    break;
+                }
+            }
+
+            firstParse = false;
+        }
+    }
+}
+
+// TODO 还要支持列引用 $key : ref value.a 这种
+public class VMap : Format {
+    public override void ParseValue(ref int column) {
+        base.ParseValue(ref column);
+        if (!finished) {
+            var keyCh = children[curChildIndex - 1];
+            var valCh = children[curChildIndex];
+            if (breakable) {
+                var vld = false;
+                Save();
+                keyCh.ParseValue(ref column);
+                if (keyCh.valid) {
+                    valCh.ParseValue(ref column);
+                    if (valCh.valid) {
+                        if (keyCh.meetEnd || valCh.meetEnd) {
+                            ForceFinish();
+                            if (valid) {
+                                vld = true;
+                                DropHistroy();
+                            } else {
+                                vld = false;
+                            }
+                        }
+                    }
+                }
+                if (!vld) {
+                    Restore();
+                    // TODO 处理 defaultMode
+                    keyTemplate.Reset();
+                    keyTemplate.ParseValue(ref column);
+                    if (!keyTemplate.valid) {
+                        valid = false;
+                        return;
+                    }
+                    template.Reset();
+                    template.ParseValue(ref column);
+                    if (!template.valid) {
+                        valid = false;
+                        return;
+                    }
+                    children.Add(keyTemplate.Clone());
+                    children.Add(template.Clone());
+                    curChildIndex += 2;
+                }
             } else {
-                state.valid = true;
-            }
-        } else {
-            state.value = this.line.cells[column].val;
-            state.finished = true;
-            state.valid = true;
-            state.breakable = true;
-        }
-        column += 1;
-    }
-}
-
-public class VTemplate : Format {
-
-    public List<Format> children = new();
-
-    public Format CurTemplate { get => children[state.curFormat]; }
-
-
-    public override void OnNewLine(Line line) {
-        base.OnNewLine(line);
-        children[state.cur].OnNewLine(line);
-    }
-
-    public override void ParseValue(ref int column) {
-        base.ParseValue(ref column);
-        var start = column;
-
-        if (!state.finished) {
-            if (state.CurChild.finished) {
-                state.Next();
-                CurTemplate.state = state.CurChild;
-            }
-            CurTemplate.ParseValue(ref column);
-            if (!state.CurChild.valid) {
-                state.valid = false;
-            }
-
-            state.breakable = state.CurChild.Breackable;
-
-            // 收集. 收集后 state 应该就可以释放掉了(或者重新利用)
-            // 这里用了隐藏条件, 在竖模板里, 如果最后一个finished, 那么前面的一定都finished了
-            if (children.Count <= state.cur && state.CurChild.finished) {
-                Collect();
-            }
-
-        }
-
-        AlignColumn(start, ref column);
-    }
-
-    public override void Collect() {
-        base.Collect();
-        var res = new List<Value>();
-        foreach (var chState in state.children) {
-            if (!chState.valid) {
-                throw new Exception();
-            }
-            if (!chState.finished) {
-                throw new Exception();
-            }
-            if (chState.collected) {
-                throw new Exception();
-            }
-            chState.collected = true;
-            res.Add(chState.value);
-        }
-        var q = from item in state.children
-                select item.value;
-        state.value = new ListValue(res);
-        state.finished = true;
-    }
-}
-
-public class VList : Format {
-    public required Format itemFormat;
-    public override void OnNewLine(Line line) {
-        base.OnNewLine(line);
-        itemFormat.OnNewLine(line);
-    }
-
-    public override void ParseValue(ref int column) {
-        base.ParseValue(ref column);
-        var start = column;
-
-        if (!state.finished) {
-            if (state.CurChild.Breackable) {
-                itemFormat.ParseValue(ref column);
-                if (!state.CurChild.valid) {
-                    CollectItem();
+                keyCh.ParseValue(ref column);
+                if (!keyCh.valid) {
+                    valid = false;
+                    return;
+                }
+                valCh.ParseValue(ref column);
+                if (!valCh.valid) {
+                    valid = false;
+                    return;
                 }
 
-            } else {
-                itemFormat.ParseValue(ref column);
-                if (!state.CurChild.valid) {
-                    state.valid = false;
+                if (keyCh.meetEnd || valCh.meetEnd) {
+                    valid = false;
+                    return;
                 }
+
             }
 
-            state.breakable = state.CurChild.Breackable;
-        } else {
-
-        }
-        AlignColumn(start, ref column);
-    }
-
-    public  void CollectItem() {
-        base.Collect();
-        if (state.value == null) {
-            state.value = new ListValue(new List<Value>());
-        }
-        var list = (state.value as ListValue).list;
-        itemFormat.Collect();
-        list.Add(state.CurChild.value);
-    }
-}
-
-public class HTemplate : Format {
-
-    public List<Format> items = new();
-
-    public override void OnNewLine(Line line) {
-        base.OnNewLine(line);
-        foreach (var item in items) {
-            item.OnNewLine(line);
-        }
-    }
-
-    public override void ParseValue(ref int column) {
-        base.ParseValue(ref column);
-        var start = column;
-
-        state.finished = true;
-        state.valid = true;
-        foreach (var item in items) {
-            item.ParseValue(ref column);
-            state.finished = state.finished && item.state.finished;
-            state.valid = state.valid && item.state.valid;
-        }
-
-        AlignColumn(start, ref column);
-
-        if (state.finished) {
-            // 收集
-            var q = from item in items
-                    select item.state.value;
-            state.value = new ListValue(q.ToList());
-        }
-
-    }
-}
-
-public class Switch : Format {
-    public bool known;
-    // 挡位. 表示当前是走的哪个分支.
-    public Value? gear;
-    public Format gearFormat;
-    public Dictionary<Value, Format> branches = new();
-
-    public override void OnNewLine(Line line) {
-        base.OnNewLine(line);
-
-        gearFormat.OnNewLine(line);
-        // 不知道用谁, 就都设置
-        foreach (var (g, b) in branches) {
-            b.OnNewLine(line);
-        }
-    }
-
-    public override void ParseValue(ref int column) {
-        base.ParseValue(ref column);
-
-        var start = column;
-        if (!gearFormat.state.finished) {
-            gearFormat.ParseValue(ref column);
-            if (!gearFormat.state.valid) { state.valid = false; } else {
-                if (gearFormat.state.finished) {
-                    gear = gearFormat.state.value;
-                }
+            var keyCh2 = children[curChildIndex - 1];
+            var valCh2 = children[curChildIndex];
+            if (keyCh2.breakable && valCh2.breakable) {
+                breakable = true;
             }
         }
-
-        if (gearFormat.state.finished) {
-            branches[gear].ParseValue(ref column);
-        }
-
-        AlignColumn(start, ref column);
     }
 }
+
--- TableConvertor/Value.cs
+++ TableConvertor/Value.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+
 namespace TableConvertor;
 
 public class Value
 {
+    public virtual Value Clone() {
+        return new Value();
+    }
+    public virtual int Count { get { return 0; } }
+    public virtual void Truncate(int end) { }
 }
 
 public class LiteralValue : Value
@@ -12,23 +19,48 @@ public class LiteralValue : Value
     {
         this.lit = lit;
     }
+    public override Value Clone() {
+        return new LiteralValue(lit);
+    }
 }
 
 public class ListValue : Value
 {
     public List<Value> list;
+    public override int Count => list.Count;
     public ListValue(List<Value> list)
     {
         this.list = list;
+    }
+    public override Value Clone() {
+        var ls = new List<Value>();
+        foreach (Value v in list) {
+            ls.Add(v.Clone());
+        }
+        return new ListValue(ls);
+    }
+    public override void Truncate(int end) {
+        list.RemoveRange(end, list.Count);
     }
 }
 
 public class MapValue : Value
 {
     public List<(Value, Value)> map;
+    public override int Count => map.Count;
     public MapValue(List<(Value, Value)> map)
     {
         this.map = map;
+    }
+    public override Value Clone() {
+        var ls = new List<(Value, Value)> ();
+        foreach (var (k, v) in map) {
+            ls.Add((k.Clone(), v.Clone()));
+        }
+        return new MapValue(ls);
+    }
+    public override void Truncate(int end) {
+        map.RemoveRange(end, map.Count);
     }
 }
 
